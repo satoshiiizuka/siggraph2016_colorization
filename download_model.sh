63722de2d93b7ffe48c4b1873da3eee32450345c
Edgar Simo-Serra <bobbens@gmail.com>
2016-04-25 12:58:57 +0900

    Check if model file exists before downloading.

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-echo "Downloading the colorization model..."
-wget "http://hi.cs.waseda.ac.jp/~iizuka/data/colornet.t7"
+if [[ ! -f 'colornet.t7' ]]; then
+   echo "Downloading the colorization model..."
+   wget "http://hi.cs.waseda.ac.jp/~iizuka/data/colornet.t7"
+else
+   echo "Model file already exists!"
+fi
