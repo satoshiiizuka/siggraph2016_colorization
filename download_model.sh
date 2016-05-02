2d31763cd8cb50c440522f4fa8747b7e458f88a0
Edgar Simo-Serra <bobbens@gmail.com>
2016-05-03 08:01:25 +0900

    Added checksums.

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# md5sum c88fa2bb6dc9f942a492a7dc7009b966
+# sha1sum d397faf0b63a173680824474bfeab4725c375ca2
+
 echo "Downloading the colorization model..."
 wget --continue -O colornet.t7 -- "http://hi.cs.waseda.ac.jp/~iizuka/data/colornet.t7"
