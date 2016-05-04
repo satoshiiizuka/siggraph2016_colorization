fac9bcdeadd5e9f2eee6839e0a5fbcd22e4dd691
Edgar Simo-Serra <bobbens@gmail.com>
2016-05-04 17:04:14 +0900

    Output should always be exact same size as input. Avoid duplicate code calls.

--- colorize.lua
+++ colorize.lua
@@ -28,11 +28,11 @@ local function pred2rgb( x, data )
    local I = torch.cat( data[1][{{1},{},{}}]:float(),
                         data[1]:clone():float():mul(2):add(-1), 1)
    local O = image.scale( I, x:size(3), x:size(2) )
-   local X = image.rgb2lab( image.yuv2rgb( torch.repeatTensor( x:clone():float():add(datamean), 3, 1, 1 ) ) )
+   local X = image.rgb2lab( image.yuv2rgb( torch.repeatTensor( x, 3, 1, 1 ) ) )
    O    = O*100
    O[1] = X[1]
    O    = image.rgb2yuv( image.lab2rgb( O ) )
-   return image.yuv2rgb( torch.cat( x:clone():float():add(datamean), O[{{2,3},{},{}}], 1 ) )
+   return image.yuv2rgb( torch.cat( x, O[{{2,3},{},{}}], 1 ) )
 end
 
 local I = image.load( infile )
@@ -44,7 +44,7 @@ X2 = X2:reshape( 1, X2:size(1), X2:size(2), X2:size(3) )
 model.forwardnodes[9].data.module.modules[3].nfeatures = X2:size(3)/8
 model.forwardnodes[9].data.module.modules[4].nfeatures = X2:size(4)/8
 
-image.save( outfile, pred2rgb( X2[1], model:forward( {X1, X2} ) ) )
+image.save( outfile, pred2rgb( I:float(), model:forward( {X1, X2} ) ) )
 
 
 
