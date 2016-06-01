e0441fb8134afbae835441ccc823d86f3a588f4b
mecab <masafumi.orz@gmail.com>
2016-06-01 18:16:16 +0900

    Reset characters color after the last message

--- download_model.sh
+++ download_model.sh
@@ -23,4 +23,4 @@ if [ "$CHECKSUM" != "$FILEMD5" ]; then
   echo "Try running this script again and if it fails remove '$FILENAME' before trying again."
   exit 1
 fi 
-echo -e "Download finished successfully! Time to \e[31mc\e[32mo\e[33m\e[34ml\e[35mo\e[36mr\e[37mi\e[91mz\e[92me\e[93m\e[94m!"
+echo -e "Download finished successfully! Time to \e[31mc\e[32mo\e[33m\e[34ml\e[35mo\e[36mr\e[37mi\e[91mz\e[92me\e[93m\e[94m!\e[m"
